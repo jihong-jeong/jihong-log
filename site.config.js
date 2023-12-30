7d516e2f76e0c5fb4704b7c4cff3334531aa44c5
jihong-jeong <121440194+jihong-jeong@users.noreply.github.com>
2023-12-30 21:51:20 +0900

    profile 변경 23.12.30 21:51

--- site.config.js
+++ site.config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "jihong-jeong",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    name: "Jihong Jeong",
+    image: "/profile.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "developer",
     bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
